17bc989f9477df5ee64b801401358590d40c4d7f
paraswtf <parasbpndeshpande@gmail.com>
2023-10-25 23:11:10 +0530

    Push for vercel

--- scripts/apidata.js
+++ scripts/apidata.js
@@ -1,6 +1,6 @@
 // Keys
 const clientId = "c6f9e1f89bf44ab0888ced219203d6bd";
-const redirectUri = "http://localhost:81/callback.html";
+const redirectUri = "https://paraswtf-spotify-clone.vercel.app";
 const scopes = [
 	"user-read-playback-state",
 	"user-modify-playback-state",
